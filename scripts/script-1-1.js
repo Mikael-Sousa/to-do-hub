a0c9b36cd2ed08c21221ace7d49c59df35444dc3
Mikael de Sousa Ribeiro <mikael.sr.dev@gmail.com>
2025-08-17 07:29:28 -0300

    update scripts/script-1-1.js

--- scripts/script-1-1.js
+++ scripts/script-1-1.js
@@ -1,21 +1,21 @@
 function addHabit() {
-    const input = document.querySelector(".input-tasks");
-    const habitName = input.value.trim();
-    const shift = document.querySelector('input[name="shift"]:checked');
-    const daysPerWeek = document.querySelector('input[name="days-per-week"]:checked');
+  const input = document.querySelector(".input-tasks");
+  const habitName = input.value.trim();
+  const shift = document.querySelector('input[name="shift"]:checked');
+  const daysPerWeek = document.querySelector('input[name="days-per-week"]:checked');
 
-    if (habitName !== "") {
-        let totalHabits = parseInt(localStorage.getItem("totalHabits")) || 0;
+  if (habitName !== "") {
+    let totalHabits = parseInt(localStorage.getItem("totalHabits")) || 0;
 
-        totalHabits++;
+    totalHabits++;
 
-        localStorage.setItem("habitName", habitName);
-        localStorage.setItem("shift", shift.value);
-        localStorage.setItem("daysPerWeek", daysPerWeek.value);
-        localStorage.setItem("totalHabits", totalHabits);
+    localStorage.setItem("habitName", habitName);
+    localStorage.setItem("shift", shift.value);
+    localStorage.setItem("daysPerWeek", daysPerWeek.value);
+    localStorage.setItem("totalHabits", totalHabits);
 
     window.location.replace("index.html")
-    } else {
-        alert("Por favor, preencha todos os campos.");
-    }
+  } else {
+    alert("Por favor, preencha todos os campos.");
+  }
 }
