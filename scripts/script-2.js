82182d04865e403d4a90c7d240c0af04a5e80360
Mikael de Sousa Ribeiro <mikael.sr.dev@gmail.com>
2025-08-17 07:24:01 -0300

    update scripts/script-2.js

--- scripts/script-2.js
+++ scripts/script-2.js
@@ -13,9 +13,8 @@ const card1 = document.querySelector(".p1");
                     localStorage.getItem("totalTasksConcluides")
                 );
 
-                card2.innerHTML =
-                    Math.round((totalTasksConcluides / totalHabits) * 100) +
-                        "%" || 0;
+                let percent = totalHabits ? Math.round((totalTasksConcluides / totalHabits) * 100) : 0;
+card2.innerHTML = percent + "%";
 
                 let perfectDays = Number(localStorage.getItem("perfectDays"));
                 card3.innerHTML = perfectDays;
